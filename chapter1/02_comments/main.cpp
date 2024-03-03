b0ca7e9ce1a44eba09a7b8faab13d08c5d542a91
Muhammad-Usama123 <osamaa.g777@gmail.com>
2024-03-04 01:19:16 +0500

    1st c++ program and starting of comments topic

---
+++ chapter1/02_comments/main.cpp
@@ -0,0 +1,20 @@
+// # include <iostream>
+
+// int main(){
+//     auto result = (10 <=> 20) > 0; // "<=>" is called spaceship operator for testing compiler supports our given (in this case c++ 20) version or not.
+//     std::cout << result << std::endl;
+// }
+
+
+// Comments:
+
+    // comments are the way to put some messages that are really meant to be read by humans.
+
+    // One line comment
+
+    /*
+    multiline comment
+    am,sndasbd
+    kjhasdhklashd
+    */
+
