6d1177f973df623318cc28a4eb6776904cc25abc
Muhammad-Usama123 <osamaa.g777@gmail.com>
2024-03-11 11:04:41 +0500

    c++ core, standard, STL

---
+++ chapter1/07_DifferenceBetween3KeyComponentsOfC++_C++CoreLangVsStandardLibraryVsSTL/main.cpp
@@ -0,0 +1,18 @@
+// # include <iostream>
+
+// int main(){
+//     auto result = (10 <=> 20) > 0; // "<=>" is called spaceship operator for testing compiler supports our given (in this case c++ 20) version or not.
+//     std::cout << result << std::endl;
+// }
+
+// C++ Core Language:
+
+    // In which we follow basic c++ rules like how variables should declare, how a function should define etc. etc.
+
+// Standard Library:
+
+    //  Set of ready to use highly specialized components that we can easily use in our c++ programs. #include <iostream>, #include <string> etc. are standard library features. c++ standard library tries to provide highly specialized components that we can use in regular c++ programs.
+
+// STL:
+
+    // Advanced version of standard library feature maybe. Containers etc. we will learn about it in this course.
